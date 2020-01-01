e3b739b03e71dbcd7be96debb4d061e5d22d4ade
FutureSaiyajin <futuresaiyajin@online.de>
2020-01-01 13:56:27 +0100

    'DOWNLOAD_DIR' variable allows you to manually set the download directory

--- WoWClassicElvUIUpdater.bat
+++ WoWClassicElvUIUpdater.bat
@@ -1,12 +1,13 @@
+SET DOWNLOAD_DIR=%CD%
 SET MYFILE=elvui.zip
 SET WOW_CLASSIC_INSTALL_DIR=C:\Program Files\World of Warcraft\_classic_
  
-IF EXIST %CD%\%MYFILE% del /f %CD%\%MYFILE%
+IF EXIST %DOWNLOAD_DIR%\%MYFILE% del /f %DOWNLOAD_DIR%\%MYFILE%
 
 curl https://www.tukui.org/classic-addons.php?download=2 -o %USERPROFILE%\Downloads\%MYFILE%
 
 for %%a in ("%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI" "%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI_OptionsUI") do rd /s /q "%%~a"
 
-PowerShell Expand-Archive -Path "%CD%\%MYFILE%" -DestinationPath '%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns'
+PowerShell Expand-Archive -Path "%DOWNLOAD_DIR%\%MYFILE%" -DestinationPath '%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns'
 
-del /s "%CD%\%MYFILE%"
+del /s "%DOWNLOAD_DIR%\%MYFILE%"
