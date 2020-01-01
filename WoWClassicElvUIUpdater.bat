4471d12a0e747d428d61e4c344ccbd2f06304cb5
FutureSaiyajin <futuresaiyajin@online.de>
2020-01-01 13:51:13 +0100

    set download path to 'current directory'

--- WoWClassicElvUIUpdater.bat
+++ WoWClassicElvUIUpdater.bat
@@ -1,12 +1,12 @@
 SET MYFILE=elvui.zip
 SET WOW_CLASSIC_INSTALL_DIR=C:\Program Files\World of Warcraft\_classic_
  
-IF EXIST %USERPROFILE%\Downloads\%MYFILE% del /f %USERPROFILE%\Downloads\%MYFILE%
+IF EXIST %CD%\%MYFILE% del /f %CD%\%MYFILE%
 
 curl https://www.tukui.org/classic-addons.php?download=2 -o %USERPROFILE%\Downloads\%MYFILE%
 
 for %%a in ("%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI" "%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI_OptionsUI") do rd /s /q "%%~a"
 
-PowerShell Expand-Archive -Path "%USERPROFILE%\Downloads\%MYFILE%" -DestinationPath '%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns'
+PowerShell Expand-Archive -Path "%CD%\%MYFILE%" -DestinationPath '%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns'
 
-del /s "%USERPROFILE%\Downloads\%MYFILE%"
+del /s "%CD%\%MYFILE%"
