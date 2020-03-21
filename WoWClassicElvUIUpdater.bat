ffac3d7a023f3962eeb799797520cc82f0b26768
FutureSaiyajin <futuresaiyajin@online.de>
2020-03-21 08:08:22 +0100

    switch file transfer method
    switch from curl to BITS

--- WoWClassicElvUIUpdater.bat
+++ WoWClassicElvUIUpdater.bat
@@ -4,7 +4,7 @@ SET WOW_CLASSIC_INSTALL_DIR=C:\Program Files\World of Warcraft\_classic_
  
 IF EXIST %DOWNLOAD_DIR%\%MYFILE% del /f %DOWNLOAD_DIR%\%MYFILE%
 
-curl https://www.tukui.org/classic-addons.php?download=2 -o %DOWNLOAD_DIR%\%MYFILE%
+PowerShell Start-BitsTransfer -Source https://www.tukui.org/classic-addons.php?download=2 -Destination %DOWNLOAD_DIR%\%MYFILE%
 
 for %%a in ("%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI" "%WOW_CLASSIC_INSTALL_DIR%\Interface\AddOns\ElvUI_OptionsUI") do rd /s /q "%%~a"
 
